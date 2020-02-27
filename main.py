7dc3811c43a1f5d7470a75457fab6c318bc571d9
zelims <zelims@users.noreply.github.com>
2020-02-26 22:53:51 -0700

    Filters out the room list for the specified times

--- main.py
+++ main.py
@@ -9,16 +9,6 @@ import time
 
 day_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ]
 
-def get_rooms():
-	rooms = driver.find_elements_by_css_selector('a[href*="/space/"] span.fc-cell-text')
-	print('Rooms available: ')
-	for room in rooms:
-		room_info = room.text.replace(')', ' ').replace('(', ' ').split(' ')
-		# 1 -- number
-		# 3 -- capacity
-
-		print(room.text.split(' ')[1])
-
 def get_day_of_week():
 	day = driver.find_element_by_class_name('fc-header-toolbar').text.split(',')[0]
 	return day_of_week.index(day)
@@ -37,44 +27,47 @@ def set_day(to):
 
 def rooms_available(floor, times):
 	room_list = driver.find_elements_by_css_selector('.fc-body tr div div div .fc-rows table tbody tr')
-	temp_rooms = defaultdict(list)
 	rooms = defaultdict(list)
+	temp_rooms = defaultdict(list)
 
 	for room in room_list:
-		# tr td div div
-			#  a.s-lc-eq-checkout -- unavailable room
-			#  a.s-lc-eq-avail -- available room
-				# a.title split by space, 0 returns the time and am/pm
 		slots = room.find_elements_by_css_selector('td div div a.fc-timeline-event')
 		for slot in slots:
 			current = slot.get_attribute('title').split(' ')
-			# 7:30am Friday, February 28, 2020 - Room 212 - Available
-			# current[0] - time
-			# current[1] - day of week
-			# current[2] - month
-			# current[3] - day of month
-			# current[4] - year
-			# current[7] - room number
-			# current[9] - status
 			if (current[7].startswith(str(floor)) or floor == 0) and current[9].lower() == "available":
-				temp_rooms[current[7]].append(current[0])
+				if times[0] <= current[0] <= times[1]:
+					temp_rooms[current[7]].append(current[0])
 
 	for room in temp_rooms:
 		for i in range(len(temp_rooms[room])):
 			if times[0] <= temp_rooms[room][i] <= times[1]:
 				rooms[room].append(temp_rooms[room][i])
+
+		if rooms[room]:
+			if len(rooms[room]) != times[2] or (rooms[room][0] != times[0] or rooms[room][-1] != times[1]):
+				try:
+					del rooms[room]
+				except KeyError:
+					continue
+
 	return rooms
 
 # -------------------
 print('Starting bot')
 
+# define variables (TODO: Read from file)
 times = ['1:00pm', '3:00pm']
-floor = -1 + 1 # second floor is the first floor imo
+floor = -1 + 1
 
 timespan = datetime.strptime(times[1], '%H:%M%p') - datetime.strptime(times[0], '%H:%M%p')
 if timespan > timedelta(hours=4):
 	print("You cannot reserve a room over 4 hours")
 	exit()
+if timespan < timedelta(hours=1):
+	print('You must reserve at least one hour')
+	exit()
+
+times.append((timespan.seconds/3600) * 2 + 1)
 
 options = Options()
 options.headless = True
@@ -85,10 +78,5 @@ print('Navigating to libcal.uccs.edu')
 set_day(day_of_week.index('Friday'))
 
 rooms = rooms_available(floor, times)
-for room in rooms:
-	print(room, ': ', end = '')
-	for i in range(len(rooms[room])):
-		print(rooms[room][i], end = ' ')
-	print('\n-----')
 
 driver.close()
