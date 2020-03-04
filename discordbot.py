3e10fa7a220fb4525b7b725d3e0011dc03f24bba
zelims <zelims@users.noreply.github.com>
2020-03-04 16:16:51 -0700

    Implemented config validation

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,19 @@
 import discord
 import os
 import webbot
-import json
 
 client = discord.Client()
+config = {}
 
 class DiscordBot:
-	def __init__(self):
-		with open('config.json') as f:
-			config = json.load(f)
+	def __init__(self, config):
 		client.run(config["token"])
 
 	@client.event
 	async def on_ready():
 		activity = discord.Game(name="type !reserve")
 		await client.change_presence(status=discord.Status.online, activity=activity)
+		print('[*] Waiting for commands')
 
 	@client.event
 	async def on_message(message):
@@ -24,11 +23,11 @@ class DiscordBot:
 		if message.content.startswith('!reserve'):
 			params = message.content.split(' ')
 			if len(params) == 5:
-				if params[3] == "1":
+				if params[4] == "1":
 					await message.channel.send("There are no study rooms on the 1st floor!")
 				else:
 					await message.channel.send("Reserving a room... please wait!")
-					bot = webbot.WebBot()
+					bot = webbot.WebBot(config)
 					await message.channel.send(bot.create(params[1], params[2], params[3], params[4]))
 			else:
 				await message.channel.send("!reserve [day of week] [start time] [end time] [floor]")
--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import discordbot
+import json
+import re
 
-print('Starting bot')
-bot = discordbot.DiscordBot()
+email_regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+
+print('[*] Starting bot')
+print('[*] Parsing config')
+with open('config.json') as f:
+	config = json.load(f)
+
+if len(config) != 6:
+	print('[!] Config is missing a field!')
+	exit()
+
+for entry in config:
+	if config[entry] == "":
+		print('[!] Config entry "{}" is empty!'.format(entry))
+		exit()
+
+if not re.search(email_regex, config["email"]):
+	print('[!] Email format in config is incorrect!')
+	exit()
+
+if len(config["initials"]) != 2:
+	print('[!] Initials format in config is incorrect!')
+	exit()
+
+bot = discordbot.DiscordBot(config)
